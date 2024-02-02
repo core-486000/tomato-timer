d56436fb2a0c8db02b451361ec67cd7bc80d1533
core <kodaira1315@gmail.com>
2024-02-02 15:27:36 +0900

    render対応

--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 # 依存関係をインストール
 yarn install
 
-# prisma/schema.prisma の内容をデータベースに反映
+# prisma/schema.prismaの内容をデータベースに反映
 npx prisma db push
+
+# Prismaクライアントを作成
+npx prisma generate
+
+# app/entry.jsのビルド
+npx webpack
 
 # アプリケーションを起動
 yarn start
