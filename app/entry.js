a308207a8ec01244a6e9ea5bfdb38b5ccc9d01b8
core <kodaira1315@gmail.com>
2024-02-03 16:18:51 +0900

    タイマーのキャンセル機能を実装

--- app/entry.js
+++ app/entry.js
@@ -11,41 +11,40 @@ dayjs.extend(isBefore);
 dayjs.tz.setDefault('Asia/Tokyo');
 
 const timer = $('#timer');
+let startButton = $('#start-button');
+let stopButton = $('#stop-button');
+let restartButton = $('#restart-button');
+const cancelButton = $('#cancel-button');
 let timerId;
 let remainingTime;
+let formattedTime;
+const workTime = 25; // タイマーの作業時間(分)
 
 // タイマーのカウントダウン
 function setTimer(finishTime) {
   const now = new Date();
 
-  // タイマーが終了したかどうか
   if (dayjs(now).isBefore(dayjs(finishTime))) {
-    console.log('タイマー動作中だよ');
-
     remainingTime = finishTime.diff(now);
-    const formattedTime = dayjs(remainingTime).tz().format('mm:ss');
+    formattedTime = dayjs(remainingTime).tz().format('mm:ss');
     timer.text(formattedTime);
     $('title').html(formattedTime);
   } else {
-    console.log('タイマー終了したよ');
   }
 }
 
-// タイマーの開始ボタンをクリックした時
 $('body').on('click', '#start-button', () => {
-  const startButton = $('#start-button');
+  startButton = $('#start-button');
   startButton.replaceWith(
     '<button id="stop-button" type="button">一時停止</button>'
   );
 
-  const minutes = 25;
-  const finishTime = dayjs(new Date()).add(minutes, 'm');
+  const finishTime = dayjs().add(workTime, 'm');
   timerId = setInterval(function(){setTimer(finishTime)}, 500);
 });
 
-// タイマーの一時停止ボタンをクリックした時
 $('body').on('click', '#stop-button', () => {
-  const stopButton = $('#stop-button');
+  stopButton = $('#stop-button');
   stopButton.replaceWith(
     '<button id="restart-button" type="button">再開</button>'
   );
@@ -53,13 +52,32 @@ $('body').on('click', '#stop-button', () => {
   clearInterval(timerId);
 });
 
-// タイマーの再開ボタンをクリックした時
 $('body').on('click', '#restart-button', () => {
-  const restartButton = $('#restart-button');
+  restartButton = $('#restart-button');
   restartButton.replaceWith(
     '<button id="stop-button" type="button">一時停止</button>'
   );
 
-  const finishTime = dayjs(new Date()).add(remainingTime, 'ms');
+  const finishTime = dayjs().add(remainingTime, 'ms');
   timerId = setInterval(function(){setTimer(finishTime)}, 500);
+});
+
+cancelButton.click(() => {
+  stopButton = $('#stop-button');
+  restartButton = $('#restart-button');
+  
+  if (stopButton.length) {
+    stopButton.replaceWith(
+      '<button id="start-button" type="button">開始</button>'
+    );
+  } else if (restartButton.length) {
+    restartButton.replaceWith(
+      '<button id="start-button" type="button">開始</button>'
+    );
+  }
+
+  remainingTime = workTime * 60 * 1000;
+  formattedTime = dayjs(remainingTime).tz().format('mm:ss');
+  timer.text(formattedTime);
+  clearInterval(timerId);
 });
