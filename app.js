195016ad08bbc853ffc9f292a49cce8c5fe6bbbd
core <kodaira1315@gmail.com>
2024-02-04 17:19:18 +0900

    タイマー終了を音でお知らせ&favicon

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient({ log: [ 'query' ] });
+const favicon = require('serve-favicon');
 require('dotenv').config();
 
 const GitHubStrategy = require('passport-github2').Strategy;
@@ -49,6 +50,7 @@ const timerRouter = require('./routes/timer');
 
 const app = express();
 app.use(helmet());
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
