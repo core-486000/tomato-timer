95567024e513c0e72ace1fffc43fe2bb6f1fdf96
core <kodaira1315@gmail.com>
2024-02-07 00:55:06 +0900

    フォームのリクエストのバリデーションを実装2

--- routes/timer.js
+++ routes/timer.js
@@ -17,10 +17,10 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/update', async (req, res, next) => {
-  await body('workTime').isInt().run(req);
-  await body('breakTime').isInt().run(req);
-  await body('loop').isInt().run(req);
-  await body('lastBreakTime').isInt().run(req);
+  await body('workTime').isInt({ min: 1, max: 59 }).run(req);
+  await body('breakTime').isInt({ min: 1, max: 59 }).run(req);
+  await body('loop').isInt({ min: 1, max: 10 }).run(req);
+  await body('lastBreakTime').isInt({ min: 1, max: 59 }).run(req);
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
